9a392876b6379eb3d62648a81ee6bc6cbd14377a
kaveesha-dilshan <kaveeshadilshan5445@gmail.com>
2026-06-14 22:29:25 +0530

    Add authcontroller

---
+++ server/src/controllers/authcontroller.js
@@ -0,0 +1,13 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        // validation 
+        
+    } catch (error) {
+        
+    }
+}
